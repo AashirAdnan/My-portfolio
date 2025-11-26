fdbb2a943c8f7fc408a8c5618ce227b7cb1a839d
aashir adnan <m.aashiradnan@gmail.com>
2025-11-26 21:13:15 +0500

    testing

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ let nav = document.querySelector(".links")
 menu.onclick = () => {
     menu.classList.toggle('bi-x');
     nav.classList.toggle('active');
-}
+};
 
-document.querySelector('form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Message sent! I will reply soon 🚀');
-    this.submit();
-});
+// Contact form code *NOT MINE
+
+
+function sendMail(){
+    let parms = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("messsage").value,
+    }
+
+    emailjs.send("service_upi7qum", "template_bf1264h", parms).then(alert("Email Sent!"));
+}
