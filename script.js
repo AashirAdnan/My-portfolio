f3091927cd3642bbb587345829f04c77567b6406
aashir adnan <m.aashiradnan@gmail.com>
2025-11-26 22:03:35 +0500

    again

--- script.js
+++ script.js
@@ -6,37 +6,6 @@ menu.onclick = () => {
     nav.classList.toggle('active');
 };
 
-// Contact form code *NOT MINE
-
-
-(function () {
-  emailjs.init("rUb4bUThmq2qGzaEG"); // ← Replace with your real Public Key
-})();
-
-window.onload = function () {
-  document
-    .getElementById("contactForm")
-    .addEventListener("submit", function (e) {
-      e.preventDefault();
-
-      this.querySelector(".contact-btn").textContent = "Sending...";
-
-      emailjs
-        .sendForm("service_upi7qum", "template_bf1264h", this)
-        .then(() => {
-          alert("Message sent! I'll reply soon");
-          this.reset();
-          this.querySelector(".contact-btn").textContent = "Submit";
-        })
-        .catch(() => {
-          alert("Failed. Try again or email me directly");
-          this.querySelector(".contact-btn").textContent = "Submit";
-        });
-    });
-};
-
-
-
 document.getElementById("message-input").addEventListener("focus", function () {
   document.querySelector(".message-box").classList.add("active");
   setTimeout(() => document.getElementById("message-textarea").focus(), 350);
